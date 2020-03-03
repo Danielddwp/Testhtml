91df2fec53a53d06c4dd6e39b4bceead60464b09
Danielddwp <48202417+Danielddwp@users.noreply.github.com>
2020-03-03 13:22:02 +0100

    Done

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -1,13 +1,42 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support;
 
 namespace Test
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        private static string DriverFolder = "C:\\Users\\Daniel\\Desktop\\Drivers1";
+        public static IWebDriver _driver;
+
+        [ClassInitialize]
+        public static void SetUp(TestContext context)
         {
+            _driver = new ChromeDriver(DriverFolder);
+        }
+
+        [ClassCleanup]
+        public static void TearDown()
+        {
+            _driver.Dispose();
+        }
+
+        [TestMethod]
+        public void TestMethod()
+        {
+            _driver.Navigate().GoToUrl("http://localhost:3000/");
+            string title = _driver.Title;
+            Assert.AreEqual("Say hello", title);
+
+            IWebElement inputElement = _driver.FindElement(By.Id("inputField"));
+
+            IWebElement button = _driver.FindElement(By.Id("button"));
+
+            IWebElement outputElement = _driver.FindElement(By.Id("outputField"));
+            string text = outputElement.Text;
+            Assert.AreEqual("Hello Daniel",text);
         }
     }
 }
